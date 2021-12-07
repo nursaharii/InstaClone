6a8a96ff37dd67491daf8ba5f83bd57b09f4f2cf
nursah <nursahari@hotmail.com>
2021-12-07 23:10:40 +0300

    Firestore'dan veri çekme işlemleri tanımlandı. Beğenme butonu counter kısmı düzenlenecek ve fotoğrafların uygulama alanını bozma sorunu düzeltilecek.

--- InstaClone/FeedCell.swift
+++ InstaClone/FeedCell.swift
@@ -6,18 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedCell: UITableViewCell {
     
     
+    @IBOutlet weak var userLbl: UILabel!
     @IBOutlet weak var counter: UILabel!
     @IBOutlet weak var comment: UILabel!
     @IBOutlet weak var img: UIImageView!
-    @IBOutlet weak var userLbl: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
+    
+    var docId = String()
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
@@ -26,5 +29,14 @@ class FeedCell: UITableViewCell {
     }
 
     @IBAction func likeBtn(_ sender: Any) {
+        let fireStoreDatabase = Firestore.firestore()
+        
+        if var sayac = Int(counter.text!){
+            sayac = sayac + 1
+            counter.text = String(sayac)
+        let likeStore = ["likes" : sayac ] as [String : Any]
+        fireStoreDatabase.collection("Posts").document(docId).setData(likeStore, merge: true)
+        }
+        
     }
 }
--- InstaClone/FeedVC.swift
+++ InstaClone/FeedVC.swift
@@ -6,28 +6,72 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
 class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return user.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedCell
-        cell.userLbl.text = "user"
-        cell.counter.text = "0"
-        cell.comment.text = "comment"
-        cell.img.image = UIImage(systemName: "square.and.arrow.up.fill")
+        cell.userLbl.text = user[indexPath.row]
+        cell.counter.text = String(counter[indexPath.row])
+        cell.comment.text = comment[indexPath.row]
+        cell.img.sd_setImage(with: URL(string: img[indexPath.row]))
+        cell.docId = docArry[indexPath.row]
         return cell
     }
+    var user = [String]()
+    var counter = [Int]()
+    var comment = [String]()
+    var img = [String]()
+    var date = [Date]()
+    var docArry = [String]()
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        getDataFromFireStore()
         // Do any additional setup after loading the view.
     }
     
+    func getDataFromFireStore(){
+        let firestoreDb = Firestore.firestore()
+        firestoreDb.collection("Posts").order(by: "date",descending: true).addSnapshotListener { snapshot, error in
+            if error != nil{
+                print(error?.localizedDescription)
+            }
+            else{
+                if snapshot?.isEmpty != nil {
+                    for doc in snapshot!.documents  {
+                        let documentID = doc.documentID
+                        self.docArry.append(documentID)
+                        if let postedBy =  doc.get("postedBy") as? String {
+                            self.user.append(postedBy)
+                        }
+                        if let imgUrl =  doc.get("imgUrl") as? String {
+                            self.img.append(imgUrl)
+                        }
+                        if let likes =  doc.get("likes") as? Int {
+                            self.counter.append(likes)
+                        }
+                        if let postComment =  doc.get("postComment") as? String {
+                            self.comment.append(postComment)
+                        }
+                        if let dates =  doc.get("date") as? Date {
+                            self.date.append(dates)
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+                
+            }
+        }
+        
+    }
 
 }
